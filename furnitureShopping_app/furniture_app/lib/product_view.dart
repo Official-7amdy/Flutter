d5f43d201492ac944fa004bdbd141c96ea370a3e
Ahmed Hamdy <142159831+Official-7amdy@users.noreply.github.com>
2024-08-19 12:25:01 +0300

    2

--- furnitureShopping_app/furniture_app/lib/product_view.dart
+++ furnitureShopping_app/furniture_app/lib/product_view.dart
@@ -9,12 +9,16 @@ class ProductView extends StatefulWidget{
     @override
     Widget build(BuildContext context) {
       return  Scaffold(
-        
+       
           body: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                SizedBox(
+
+                Stack(
+                  children:[ 
+                   
+                    Container(
                   height: MediaQuery.sizeOf(context).height / 2,
                   width: double.infinity,
                   
@@ -26,18 +30,38 @@ class ProductView extends StatefulWidget{
                       image: DecorationImage(
                         image: AssetImage('assets/images/product1.jpeg'),
                         fit: BoxFit.cover,
+                        
                       ))
                   ),
+                                    
+                                    
+                  ),
+                  Column(
+                    children: [
+                       SizedBox(
+                    height:15,
+                  ),
+                   Padding(
+                     padding: const EdgeInsets.all(20.0),
+                     child: Container(
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.all(Radius.circular(5)),
+                        color: Color.fromARGB(255, 255, 255, 255)
+                      ),
+                      child: IconButton(
+                        
+                        onPressed: (){}, icon: const Icon(Icons.arrow_back_ios,color: Color.fromARGB(255, 0, 0, 0),))),
+                   ),
 
-
-                )
+                    ],
+                  )
+                 
+                             ]               )
               ], 
              
                       ),
                       ),
                       );
                       }
-}
-
-class ProductList {
 }
