ded21aae6be2d307a2cf00c687152c1e4659cb2e
Ahmed Hamdy <142159831+Official-7amdy@users.noreply.github.com>
2024-09-23 00:32:01 +0300

    Update bookmarks_view.dart

--- Bookia/bookia_app/lib/feature/Bookmarks/View/bookmarks_view.dart
+++ Bookia/bookia_app/lib/feature/Bookmarks/View/bookmarks_view.dart
@@ -1,5 +1,8 @@
+import 'package:bookia_app/core/customWidgets/customButton.dart';
+import 'package:bookia_app/core/utils/appColors.dart';
 import 'package:bookia_app/feature/Bookmarks/Widgets/bookmarkAppBar.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class BookmarksView extends StatefulWidget {
   const BookmarksView({super.key});
@@ -13,6 +16,83 @@ class _BookmarksViewState extends State<BookmarksView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: bookmarkAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: ListView.separated(
+          itemCount: 2, // You can dynamically change the item count here
+          separatorBuilder: (BuildContext context, int index) {
+            return Divider(thickness: 3, color: Colors.grey[300]);
+          },
+          itemBuilder: (BuildContext context, int index) {
+            return Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(
+                    width: 120,
+                    height: 150,
+                    'https://picsum.photos/200/300',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(width: 12), // Add spacing between the image and text
+                 Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Gap(20),
+                      Text(
+                        'Book Title',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 8), // Add some space between title and author
+                      Text(
+                        'Book Author',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Book Description',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey,
+                        ),
+                      ),
+                     
+                    ],
+                  ),
+                ),
+                
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        // Implement remove action
+                      },
+                      child: const Text(
+                        'Remove From Cart',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: AppColors.redColor,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            );
+          },
+        ),
+      ),
     );
   }
 }
